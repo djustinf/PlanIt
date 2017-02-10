28c9e5c0fcfb3f0aad89405d34cdf472fcf41fc1
Kris Campos <kcampos0101@gmail.com>
2017-02-10 11:52:20 -0800

    Finished creating example web server and REST API

---
+++ Business Logic/src/Models/HelloApp.java
@@ -0,0 +1,15 @@
+package Models;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Kris on 2/10/2017.
+ */
+@SpringBootApplication
+public class HelloApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApp.class, args);
+    }
+}
