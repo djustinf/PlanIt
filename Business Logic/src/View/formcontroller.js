eb5cfb9e542dc951eae33cd701f67f9dfe4edf11
Sonia Narayanan <sonianara96@gmail.com>
2017-02-11 15:24:53 -0800

    filters

--- Business Logic/src/View/formcontroller.js
+++ Business Logic/src/View/formcontroller.js
@@ -3,9 +3,9 @@ app.controller('myCtrl', function($scope) {
     $scope.years = ["Year", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"];
     $scope.terms = ["Term", "Fall", "Winter", "Spring"];
     $scope.views = ["View", "Week View", "List View"];
-    $scope.courseLevels = ["100", "200", "300", "400", "500"];
-    $scope.days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    $scope.faculty = ["Bob", "John", "Sally", "Susie"];
-    $scope.classes = ["101", "102", "103"];
-    $scope.times = ["Morning", "Afternoon", "Evening", "Night"];
+    $scope.courseLevels = ["Course Level", "100", "200", "300", "400", "500"];
+    $scope.days = ["Day", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    $scope.faculty = ["Faculty", "Bob", "John", "Sally", "Susie"];
+    $scope.classes = ["Class", "101", "102", "103"];
+    $scope.times = ["Time", "Morning", "Afternoon", "Evening", "Night"];
 });
