84448779d5512a97445e9050963f663c95f1c8ce
Landon Woollard <landonwoollard@gmail.com>
2017-02-11 13:35:41 -0800

    Changes to ID and Constructor

--- Business Logic/src/Models/Scheduling/Comment.java
+++ Business Logic/src/Models/Scheduling/Comment.java
@@ -4,22 +4,25 @@ package Models.Scheduling;
  * Created by Kris on 2/3/2017.
  */
 public class Comment {
-    private long commentID;
+    private long ID;
     private int posterID;
     private String comment;
 
     public Comment(int posterID, String comment){
         this.posterID = posterID;
         this.comment = comment;
-        commentID = System.nanoTime(); //what are the odds we assign 2 comments the same id?
+    }
+
+    public setID(int ID) {
+        this.ID = ID;
     }
 
     public int getPosterID(){
         return posterID;
     }
 
-    public int getCommentID(){
-        return commentID;
+    public int getID(){
+        return ID;
     }
 
     public String getComment(){
