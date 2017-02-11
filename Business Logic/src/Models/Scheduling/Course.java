2a84aea1b764959028c314f0424bf5376d168efa
Justin Foxhoven <djustinf@gmail.com>
2017-02-11 12:07:06 -0800

    Outlined Course. Working on CourseOffering

--- Business Logic/src/Models/Scheduling/Course.java
+++ Business Logic/src/Models/Scheduling/Course.java
@@ -1,7 +1,46 @@
 package Models.Scheduling;
 
+import Models.People.Faculty;
+import java.util.ArrayList;
+
 /**
  * Created by Kris on 2/3/2017.
  */
 public class Course {
+
+    private int ID;
+    private String name;
+    private ArrayList<CourseOffering> offerings;
+    private ArrayList<Faculty> faculty;
+
+    public Course(int ID, String name) {
+        this.ID = ID;
+        this.name = name;
+        faculty = new ArrayList<Faculty>();
+        offerings = new ArrayList<CourseOffering>();
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addFaculty(Faculty member) {
+        faculty.add(member);
+    }
+
+    public void removeFaculty(Faculty member) {
+        faculty.remove(member);
+    }
+
+    public void addOffering(CourseOffering offer) {
+        offerings.add(offer);
+    }
+
+    public void removeOffering(CourseOffering offer) {
+        offerings.remove(offer);
+    }
 }
---
+++ Business Logic/src/Models/Scheduling/CourseOffering.java
@@ -0,0 +1,9 @@
+package Models.Scheduling;
+
+/**
+ * Created by Justin on 2/11/2017.
+ */
+public class CourseOffering {
+
+
+}
