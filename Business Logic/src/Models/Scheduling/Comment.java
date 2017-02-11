9d16935cdfe4d00072acc054484d3383103ad5ad
Landon Woollard <landonwoollard@gmail.com>
2017-02-11 13:27:39 -0800

    Added constructor and getters to an otherwise empty shell

--- Business Logic/src/Models/Scheduling/Comment.java
+++ Business Logic/src/Models/Scheduling/Comment.java
@@ -4,4 +4,25 @@ package Models.Scheduling;
  * Created by Kris on 2/3/2017.
  */
 public class Comment {
+    private int commentID;
+    private int posterID;
+    private String comment;
+
+    public Comment(int posterID, String comment){
+        this.posterID = posterID;
+        this.comment = comment;
+        commentID = System.nanoTime(); //what are the odds we assign 2 comments the same id
+    }
+
+    public int getPosterID(){
+        return posterID;
+    }
+
+    public int getCommentID(){
+        return commentID;
+    }
+
+    public String getComment(){
+        return comment;
+    }
 }
