4828058f6133f89da994f44f07b69d25f5e1f8bb
Landon Woollard <landonwoollard@gmail.com>
2017-02-11 13:17:26 -0800

    component stuff

--- Business Logic/src/Models/Scheduling/Component.java
+++ Business Logic/src/Models/Scheduling/Component.java
@@ -7,7 +7,35 @@ public class Component {
     private int sectionID;
     private String sectionType;
     private int workUnits;
+    private int startTime;
     private double classHours;
 
+    public Component(int sectionID, String sectionType, int workUnits,
+                     int startTime, double classHours){
+        this.sectionID = sectionID;
+        this.sectionType = sectionType;
+        this.workUnits = workUnits;
+        this.startTime = startTime;
+        this.classHours = classHours;
+    }
 
+    public int getSectionID(){
+        return sectionID;
+    }
+
+    public String getSectionType(){
+        return sectionType;
+    }
+
+    public int getWorkUnits() {
+        return workUnits;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public double getClassHours() {
+        return classHours;
+    }
 }
