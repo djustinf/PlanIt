0457363083dcc36b58121d46747e8487dc66d4e5
Landon Woollard <landonwoollard@gmail.com>
2017-02-11 16:07:27 -0800

    Adding days into components for use in room offerings

--- Business Logic/src/Models/Scheduling/RoomOffering.java
+++ Business Logic/src/Models/Scheduling/RoomOffering.java
@@ -6,9 +6,21 @@ import java.util.List;
  * Created by Landon on 2/11/2017.
  */
 public class RoomOffering {
-    private List<List<Integer>> availability;
+    private Room parent;
+    private int[][] availability;
+    private List<Component> components;
 
-    public RoomOffering(){
+    private static final int DAYS_IN_WEEK = 7;
+    private static final int INTERVALS_PER_DAY = 48;
+
+    public RoomOffering(Room parent){
+        this.parent = parent;
+        availability = new int[DAYS_IN_WEEK][INTERVALS_PER_DAY];
+        components = new List<Component>();
+    }
+
+    public void addComponent(Component c){
+        components.add(c);
 
     }
 
