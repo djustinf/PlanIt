24e3fbb6f331b82bdb3594fecbe873f5e7ec3a28
Sonia Narayanan <sonianara96@gmail.com>
2017-02-11 17:03:09 -0800

    pages

--- Business Logic/src/View/formcontroller.js
+++ Business Logic/src/View/formcontroller.js
@@ -1,11 +1,37 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-    $scope.years = ["Year", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"];
-    $scope.terms = ["Term", "Fall", "Winter", "Spring"];
-    $scope.views = ["View", "Week View", "List View"];
-    $scope.courseLevels = ["Course Level", "100", "200", "300", "400", "500"];
-    $scope.days = ["Day", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    $scope.faculty = ["Faculty", "Bob", "John", "Sally", "Susie"];
-    $scope.classes = ["Class", "101", "102", "103"];
-    $scope.times = ["Time", "Morning", "Afternoon", "Evening", "Night"];
+app.controller('myCtrl', function ($scope) {
+    $scope.menus = [
+        {
+            name: "Year",
+            values: ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"]
+        },
+        {
+            name: "Terms",
+            values: ["Fall", "Winter", "Spring"]
+        },
+        {
+            name: "Views",
+            values: ["Week View", "List View"]
+        },
+        {
+            name: "Course Levels",
+            values: ["100", "200", "300", "400", "500"]
+        },
+        {
+            name: "Days",
+            values: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        },
+        {
+            name: "Faculty",
+            values: ["Course Level", "100", "200", "300", "400", "500"]
+        },
+        {
+            name: "Classes",
+            values: ["101", "102", "103"]
+        },
+        {
+            name: "Time",
+            values: ["Morning", "Afternoon", "Evening", "Night"]
+        },
+    ]
 });
