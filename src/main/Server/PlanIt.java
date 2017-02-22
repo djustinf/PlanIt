14201058bdb8bf2a3f299f8588ad9ed4b7fec1a3
Landon Woollard <landonwoollard@gmail.com>
2017-02-22 10:47:57 -0800

    Some user services added

--- src/main/Server/PlanIt.java
+++ src/main/Server/PlanIt.java
@@ -3,7 +3,6 @@ package Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Launching point for server
