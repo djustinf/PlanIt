014ab772e471fcd58f660f5b31e4e7c8f304c45f
Justin Foxhoven <djustinf@gmail.com>
2017-02-27 23:47:05 -0800

    Very janky security

---
+++ src/main/Server/Security/SecurityConfig.java
@@ -0,0 +1,67 @@
+package Server.Security;
+
+import Server.Requests.PersistenceFactory;
+import Server.Requests.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ * Created by Justin on 2/27/2017.
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends GlobalAuthenticationConfigurerAdapter {
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                EntityManagerFactory singleton = PersistenceFactory.getInstance().getEntityManagerFactory();
+                EntityManager entityManager = singleton.createEntityManager();
+                Models.People.User account = UserService.getUser(entityManager, username);
+                if (account != null) {
+                    return new User(account.getUserName(), account.getPassword(), true, true, true, true,
+                            AuthorityUtils.createAuthorityList("USER"));
+                } else {
+                    throw new UsernameNotFoundException("could not find the user '"
+                            + username + "'");
+                }
+            }
+
+        };
+    }
+}
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().fullyAuthenticated().and().
+                httpBasic().and().
+                csrf().disable();
+    }
+}
